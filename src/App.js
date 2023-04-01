df41244fe5359eedcc82f17c748f49ebcca4f369
Arnoud-Wilson <arnoud608@gmail.com>
2023-04-01 15:22:55 +0200

    getData gemaakt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import Button from "./components/Button";
+import GetAllData from "./components/GetData";
 import './App.css';
+import Pokemon from "./assets/International_Pokémon_logo.svg.png"
 
 function App() {
+
   return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
+      <>
+        <img className="pokemon-logo" src={ Pokemon }  alt="pokemon logo"/>
+        {/*<div className="button-wrapper" >*/}
+        {/*    <Button*/}
+        {/*        name="vorige"*/}
+        {/*    />*/}
+        {/*    <Button*/}
+        {/*        name="volgende"*/}
+        {/*        onClick={GetAllData}*/}
+        {/*    />*/}
+        {/*</div>*/}
+
+        <GetAllData />
+
+      </>
+
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import "./button.css";
+//
+// function Button({name, onClick}) {
+//
+//     return (
+//                 <button onClick={onClick}>{name}</button>
+//     );
+// }
+//
+// export default Button;
